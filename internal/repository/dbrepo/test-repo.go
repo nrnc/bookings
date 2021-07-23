a7a7119e4397f16d30a63b02036cadcf41bef28f
nchukkaio <narendra.chukka@yahoo.in>
2021-07-23 14:28:41 +0530

    added check for room id having more than 2 for test db repo

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -1,6 +1,7 @@
 package dbrepo
 
 import (
+	"errors"
 	"time"
 
 	"github.com/nchukkaio/goweblearning/internal/models"
@@ -34,5 +35,8 @@ func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]mode
 
 func (m *testDBRepo) GetRoomById(id int) (models.Room, error) {
 	var room models.Room
+	if id > 2 {
+		return room, errors.New("room with id's more than 2 doesn't exists")
+	}
 	return room, nil
 }
